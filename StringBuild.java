778ccb6bda09b262867fbe8fac9f74f68f25d894
chetan kanade <chetankanade8@gmail.com>
2023-05-27 23:58:27 +0530

    StringBuilder

---
+++ StringBuild.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class StringBuild {
+    public static void main(String arg[]) {
+        StringBuilder sb = new StringBuilder("Chetan");
+        System.out.println(sb);
+
+    }
+}
