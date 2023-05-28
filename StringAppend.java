1c9a9aef14c0926c1d22a0a614b24c5ddbb05e64
chetan kanade <chetankanade8@gmail.com>
2023-05-28 23:49:47 +0530

    string methods

---
+++ StringAppend.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class StringAppend {
+    public static void main(String arg[]) {
+        StringBuilder sb = new StringBuilder("h");
+        // this will add char at the end
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        System.out.println(sb);
+    }
+
+}
--- StringBuild.java
+++ StringBuild.java
@@ -2,8 +2,22 @@ import java.util.*;
 
 public class StringBuild {
     public static void main(String arg[]) {
-        StringBuilder sb = new StringBuilder("Chetan");
+        StringBuilder sb = new StringBuilder("Hello Chetan");
+
+        //this will print 9th index char
+        System.out.println(sb.charAt(9));
+ 
+        //this will remove and add new char at given index
+        sb.setCharAt(0,'p'); 
         System.out.println(sb);
+
+        //this will add char at given index 
+        sb.insert(2, 'l');
+        System.out.println(" l inserted --> " + sb);
+
+        //this will delete (from - to) index char 
+        sb.delete(3, 4);
+        System.out.println("delete from 3rd index to 4th index --> " + sb);
 
     }
 }
