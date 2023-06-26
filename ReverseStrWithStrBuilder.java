f5480b5fb2d2ad51e897d8b329824a11a93bb5b6
chetan kanade <chetankanade8@gmail.com>
2023-06-26 23:54:48 +0530

    reverse str with str builder

---
+++ ReverseStrWithStrBuilder.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ReverseStrWithStrBuilder {
+    public static void main(String arg[]) {
+
+        StringBuilder sb = new StringBuilder("hello");
+        for (int i = 0; i < sb.length() / 2; i++) {
+            int front = i;
+            int back = sb.length() - 1 - i;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+        System.out.println(sb);
+    }
+
+}
